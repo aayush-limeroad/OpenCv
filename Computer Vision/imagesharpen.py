7f95c8427579a81a27dc1f941932599aa4f39697
aayush-limeroad <aayusha@limeroad.com>
2023-02-07 18:33:57 +0530

    removed time sleep

--- Computer Vision/imagesharpen.py
+++ Computer Vision/imagesharpen.py
@@ -20,18 +20,18 @@ def sharpen_one(lines,index):
     resource = urllib.request.urlopen(lines)
                 
                 # input_loc="/media/aayush/3236805D368023C7/Users/aayus/Desktop/Work/Computer Vision/input"
-    input = open("./input/image-{}.jpg".format(index),"wb")
+    input = open("./Computer Vision/input/image-{}.jpg".format(index),"wb")
     input.write(resource.read())
     input.close()
 
             
-    img=cv2.imread("./input/image-{}.jpg".format(index))
+    img=cv2.imread("./Computer Vision/input/image-{}.jpg".format(index))
     kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]]) #why this?  multithreading in python  how to put images in s3 bucket and how many can we put in one
     im = cv2.filter2D(img, -1, kernel)
                 
     # output_loc="/media/aayush/3236805D368023C7/Users/aayus/Desktop/Work/Computer Vision/output"
-    cv2.imwrite("./output/image-{}.jpg".format(index),im)
-    time.sleep(0.5)
+    cv2.imwrite("./Computer Vision/output/image-{}.jpg".format(index),im)
+    # time.sleep(0.2)
     
     
 def sharpen_all():
